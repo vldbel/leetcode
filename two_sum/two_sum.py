df4034fc0d91c8d01e5f42a5626f2b2b81621488
vldbel <vldbel@gmail.com>
2024-06-09 16:45:33 +0000

    some more assertions

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -8,7 +8,7 @@ def two_nums(nums: list, target: int) -> list:
         valid_nums[num] = idx
 # complexity: O(n), size: O(n) 
 
-
+assert two_nums([2, 7, 11, 15, 16, 28, 12, 6, 2 , 1], 21) == [3, 7]
 assert two_nums([2, 7, 11, 15], 9) == [0, 1]
 assert two_nums([3, 2, 4], 6) == [1, 2]
 assert two_nums([3, 3], 6) == [0, 1]
