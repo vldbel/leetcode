f615ddde6f3a11e14fc7dcb39b6ce50a805d3882
vldbel <vldbel@gmail.com>
2024-06-10 08:08:58 +0000

    remove dublicates - update

--- remove_duplicates_from_array/remove_duplicates_from_array.py
+++ remove_duplicates_from_array/remove_duplicates_from_array.py
@@ -1,15 +1,13 @@
 def remove_duplicates(nums:list) -> tuple:
-    for pos in range(1, len(nums)-1):
-        for car in range(1, len(nums)-1):
+    for pos in range(1, len(nums) - 1):
+        for car in range(1, len(nums) - 1):
             if nums[car-1] == nums[car] and nums[car] != nums[car+1]:
                 nums[car] = nums[car+1]
     unique_nums = len(set(nums))
     return unique_nums, nums[:unique_nums] 
-        
+    
+    # not happy with that solution
+    # It is not clear from description if I can use additional "arrays" to store data to reduce itrations count.        
 
-
-
-# print(remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
-               #  [0, 1, 1, 1, 2, 2, 3, 3, 4, 4]
 assert remove_duplicates([1, 1, 2]) == (2, [1, 2])
 assert remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == (5, [0, 1, 2, 3, 4])
