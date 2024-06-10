0723d9d8fe5ce38667f267f974c7d11f1608d500
vldbel <vldbel@gmail.com>
2024-06-10 21:07:32 +0000

    bugfixes

--- longest_common_prefix/longest_common_prefix.py
+++ longest_common_prefix/longest_common_prefix.py
@@ -2,7 +2,7 @@ def longest_common_prefix(string_list:list) -> str:
     shortest_string = min(string_list, key=len)
     shortest_string_len = len(shortest_string)
     
-    for pos in range(shortest_string_len-1):
+    for pos in range(shortest_string_len):
         if not all(map(lambda x : x[pos] == shortest_string[pos], string_list)):
             return shortest_string[:pos]
     return shortest_string
@@ -13,3 +13,4 @@ assert longest_common_prefix(['abc', 'ab', 'a']) == 'a'
 assert longest_common_prefix(['flower', 'flow', 'flowcontrol']) == 'flow'
 assert longest_common_prefix(['jam', 'jammy', 'jammmieee']) == 'jam'
 assert longest_common_prefix(['dog', 'racecar', 'car']) == ''
+assert print(longest_common_prefix(["a","b"])) == ''
