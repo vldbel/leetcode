3dd43dc7498e007b78e7682926ac27567909cbaa
vldbel <vldbel@gmail.com>
2024-06-12 21:09:36 +0000

    improved version

--- remove_duplicates_from_array/remove_duplicates_from_array.py
+++ remove_duplicates_from_array/remove_duplicates_from_array.py
@@ -1,13 +1,19 @@
-def remove_duplicates(nums:list) -> tuple:
-    for pos in range(1, len(nums) - 1):
-        for car in range(1, len(nums) - 1):
-            if nums[car-1] == nums[car] and nums[car] != nums[car+1]:
-                nums[car] = nums[car+1]
-    unique_nums = len(set(nums))
-    return unique_nums, nums[:unique_nums] 
-    
-    # not happy with that solution
-    # It is not clear from description if I can use additional "arrays" to store data to reduce itrations count.        
+def remove_duplicates(nums:list) -> int:
+    last_registered_num = nums[0] # the first element always keeps its place 
+    insert_cursor = 1 # cursor to track place to insert new element 
 
-assert remove_duplicates([1, 1, 2]) == (2, [1, 2])
-assert remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == (5, [0, 1, 2, 3, 4])
+    for looking_cursor in range(1, len(nums)):        
+        current_num = nums[looking_cursor]
+        
+        if current_num == last_registered_num:
+            continue  # num is already registered
+        
+        nums[insert_cursor] = current_num
+        last_registered_num = current_num
+        insert_cursor += 1
+            
+    return insert_cursor
+
+                
+assert remove_duplicates([1, 1, 2]) == 2
+assert remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == 5
